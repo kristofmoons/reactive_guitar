198e2a059fe2e48c44316a86ab704700446c583f
kristofmoons <r0696321@student.thomasmore.be>
2022-11-08 19:02:13 +0100

    form now sends mails

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -2,34 +2,49 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export function FormInput() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_2dk9k98', 'template_wsjyke7', form.current, 'kzBBe_4NASng7Cd_7')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
-        <Form>
+        <Form ref={form} onSubmit={sendEmail} >
             <Row className="mb-3">
                 <Form.Group as={Col} controlId="formGridEmail">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" name="user_email"  placeholder="Enter email" />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridPassword">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="name" placeholder="Name" />
+                    <Form.Control type="text" name="user_name"  placeholder="Name" />
                 </Form.Group>
             </Row>
 
             <Form.Group className="mb-3" controlId="formGridAddress1">
                 <Form.Label>Subject</Form.Label>
-                <Form.Control placeholder="Subject" />
+                <Form.Control type="text" name="subject" placeholder="Subject" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formGridAddress2">
                 <Form.Label>Contact</Form.Label>
-                <Form.Control placeholder="I have tabs, found a bug" />
+                <Form.Control name="message" placeholder="I have tabs, found a bug" />
             </Form.Group>
 
 
-            <Button variant="dark" type="submit">
+            <Button variant="dark" type="submit" value="Send">
                 Submit
             </Button>
         </Form>
