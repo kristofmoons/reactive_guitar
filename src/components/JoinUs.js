5f197d590646f0a6d8bd7c6d760a944d45fa29cf
kristofmoons <r0696321@student.thomasmore.be>
2022-11-09 22:03:17 +0100

    added more stuf in about

---
+++ src/components/JoinUs.js
@@ -0,0 +1,57 @@
+
+export function JoinUs() {
+
+    return (
+
+        <section id="sec-pricing" className="sec-pricing" style={{marginBottom:"2vh"}}>
+            <div className="container">
+                <h1 className="h4 mb-5 text-center">Join us now!</h1>
+
+                <div className="row justify-content-center">
+                    <div className="col-md-4">
+                        <div className="card text-center">
+                            <div className="card-block">
+                                <h4 className="card-title h5">Just for Music</h4>
+
+                                <h5 className="card-title h6">10 &euro;/year</h5>
+
+                                <p className="card-text">The flexible place to grow your music skills</p>
+
+                                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" className="btn btn-primary" style={{marginBottom:"2vh"}}>Find out more</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4">
+                        <div className="card text-center">
+                            <div className="card-block">
+                                <h4 className="card-title h5">Small Time Rocker</h4>
+
+                                <h5 className="card-title h6">20 &euro;/year</h5>
+
+                                <p className="card-text">Your own personal feeling in an open space</p>
+
+                                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" className="btn btn-primary" style={{marginBottom:"2vh"}} >Find out more</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4">
+                        <div className="card text-center">
+                            <div className="card-block justify-content-center">
+                                <h4 className="card-title h5">Guitar God</h4>
+
+                                <h5 className="card-title h6">69 &euro;/year</h5>
+
+                                <p className="card-text">perfection or nothing is how you live</p>
+
+                                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" className="btn btn-primary" style={{marginBottom:"2vh"}} >Find out more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+
+}
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,13 @@
 import {Landing} from "../components/Landing";
 import {About} from "../components/About";
+import {JoinUs} from "../components/JoinUs";
 
 export default function LandingPage() {
     return (
         <>
             <Landing/>
             <About/>
+            <JoinUs/>
         </>
     );
 }
