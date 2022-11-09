072cbf65374801dd5df6c93813e5bc21b783b531
kristofmoons <r0696321@student.thomasmore.be>
2022-11-09 16:32:11 +0100

    new event handler

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,15 +1,30 @@
 import {Card} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import {useState} from "react";
+
+function OpenCloseButton(props) {
+    const {isOpen, onChangeIsOpen} = props;
+
+    return <Button
+        variant="outline-primary" size="sm"
+        onClick={() => onChangeIsOpen(!isOpen)}>
+        {isOpen ? "-" : "+"}
+    </Button>;
+}
 
 export function MyCard(props) {
     const {title, children} = props;
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={`images/CountryRoads.png`} />
+        <Card style={{width: '18rem'}}>
+            <Card.Img variant="top" src={`images/CountryRoads.png`}/>
             <Card.Body>
+                <OpenCloseButton isOpen={isOpen} onChangeIsOpen={() => setIsOpen(!isOpen)}/>
                 <Card.Title>{title}</Card.Title>
-                <Card.Text>
+                {isOpen && <Card.Text>
                     {children}
-                </Card.Text>
+                </Card.Text>}
+
             </Card.Body>
         </Card>
     );
