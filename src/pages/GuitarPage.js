6dc644d20f150fd4e82805356521c422160391e7
kristofmoons <r0696321@student.thomasmore.be>
2022-11-09 15:18:51 +0100

    edit guitar .3 does not work yet

--- src/pages/GuitarPage.js
+++ src/pages/GuitarPage.js
@@ -1,4 +1,4 @@
-import {collection} from 'firebase/firestore'
+import {collection, updateDoc} from 'firebase/firestore'
 import {firestoreDB} from "../services/firebase";
 import {useCollectionData} from "react-firebase-hooks/firestore";
 import {Guitars} from "../components/Guitars";
@@ -8,26 +8,51 @@ import {Form, Modal} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
 const guitarConverter = {
-    toFirestore: undefined,
+    toFirestore: function (dataInApp) {
+        return {
+            kind: dataInApp.kind,
+            strings: dataInApp.strings,
+            genre: dataInApp.genre,
+        };
+    },
     fromFirestore: function (snapshot, options) {
         const data = snapshot.data(options);
-        return {...data, id: snapshot.id}
+        return {...data, id: snapshot.id, ref: snapshot.ref}
     }
 };
 
 function GuitarFormEdit(props) {
     const {guitar, onClose, onSaveGuitar} = props;
-    console.log({guitar})
+    const [guitarToEdit, setGuitarToEdit] = useState({...guitar});
     return (
         <Modal show={!!guitar} onHide={onClose}>
             <Modal.Header closeButton>
-                {guitar && <div>You are editing: {guitar.kind}</div>}
+                <h3 className="ms-2">Edit Guitar</h3>
             </Modal.Header>
-            <div className="d-flex justify-content-center p-2">
-                <Button className="m-1" size="lg" onClick={() => onClose()}>cancel</Button>
-                <Button className="m-1" size="lg" onClick={ async () => {
-                    onSaveGuitar(guitar);
-                }}>save</Button>
+            <div className="m-3 mt-0">
+                <div className="d-flex justify-content-end fs-6 fw-light">
+                    {guitar.id}
+                </div>
+                <Form>
+                    <Form.Label className="mt-2 ms-1">kind of guitar:</Form.Label>
+                    <Form.Control
+                        value={guitarToEdit.kind}
+                        onChange={e => setGuitarToEdit({...guitarToEdit, name: e.target.value})}/>
+                    <Form.Label className="mt-2 ms-1">kind:</Form.Label>
+                    <Form.Control
+                        value={guitarToEdit.strings}
+                        onChange={e => setGuitarToEdit({...guitarToEdit, strings:e.target.value})}/>
+                    <Form.Label className="mt-2 ms-1">genre:</Form.Label>
+                    <Form.Control
+                        value={guitarToEdit.genre}
+                        onChange={e => setGuitarToEdit({...guitarToEdit, genre: e.target.value})}/>
+                </Form>
+                <div className="d-flex justify-content-center p-2">
+                    <Button className="m-1" size="lg" onClick={() => onClose()}>cancel</Button>
+                    <Button className="m-1" size="lg" onClick={async () => {
+                        if (await onSaveGuitar(guitarToEdit)) onClose();
+                    }}>save</Button>
+                </div>
             </div>
         </Modal>
     );
@@ -46,9 +71,15 @@ export default function GuitarPage() {
     }
 
     async function editGuitarSave(guitar) {
-        console.log(`editing guitar ${guitar.kind}`)
-            }
-
+        try {
+            await updateDoc(guitar.ref, guitar);
+            console.log(`save guitar ${guitar.kind} DONE`);
+            return true;
+        } catch (e){console.error(e)
+            console.log(`ERROR save guitar ${guitar.kind} NOT done`);
+        }
+        return false;
+    }
     console.log(values)
     return (
 
@@ -57,7 +88,6 @@ export default function GuitarPage() {
             <FilterSlider>
                 {
                     <Form>
-
                         <Form.Label>search</Form.Label>
                         <Form.Control
                             value={search}
@@ -68,7 +98,7 @@ export default function GuitarPage() {
                 {guitarSelected && <GuitarFormEdit
                     guitar={guitarSelected}
                     onClose={() => setGuitarSelected(undefined)}
-                    onSavePerson={() => editGuitarSave(guitarSelected)}/>}
+                    onSaveGuitar={editGuitarSave}/>}
             <Guitars guitars={values?.filter(g => g.kind.includes(search))}  onEditGuitar={editGuitar} title={"Guitars"}/>
 
         </div>
