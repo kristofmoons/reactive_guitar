6eecd869261a18b4aa482c705b542f09b00c0e81
kristofmoons <r0696321@student.thomasmore.be>
2022-11-09 14:05:01 +0100

    edit guitar .2

--- src/pages/GuitarPage.js
+++ src/pages/GuitarPage.js
@@ -4,7 +4,8 @@ import {useCollectionData} from "react-firebase-hooks/firestore";
 import {Guitars} from "../components/Guitars";
 import {FilterSlider} from "../components/FilterSlider";
 import {useState} from "react";
-import {Form} from "react-bootstrap";
+import {Form, Modal} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 const guitarConverter = {
     toFirestore: undefined,
@@ -14,17 +15,41 @@ const guitarConverter = {
     }
 };
 
+function GuitarFormEdit(props) {
+    const {guitar, onClose, onSaveGuitar} = props;
+    console.log({guitar})
+    return (
+        <Modal show={!!guitar} onHide={onClose}>
+            <Modal.Header closeButton>
+                {guitar && <div>You are editing: {guitar.kind}</div>}
+            </Modal.Header>
+            <div className="d-flex justify-content-center p-2">
+                <Button className="m-1" size="lg" onClick={() => onClose()}>cancel</Button>
+                <Button className="m-1" size="lg" onClick={ async () => {
+                    onSaveGuitar(guitar);
+                }}>save</Button>
+            </div>
+        </Modal>
+    );
+}
+
 export default function GuitarPage() {
     const collectionRef = collection(firestoreDB, 'guitars').withConverter(guitarConverter);
     const [values, loading, error] = useCollectionData(collectionRef);
     const [search, setSearch] = useState("");
+    const [guitarSelected, setGuitarSelected] = useState();
     console.log({values, loading, error});
 
     function editGuitar(guitar) {
-        console.log(`editing guitar ${guitar.kind}`)
+        console.log(`editing guitar ${guitar.kind}`);
+        setGuitarSelected(guitar);
     }
 
+    async function editGuitarSave(guitar) {
+        console.log(`editing guitar ${guitar.kind}`)
+            }
 
+    console.log(values)
     return (
 
             <div style={{backgroundImage: "url('./images/landing1.png')", backgroundRepeat:"no-repeat",
@@ -40,6 +65,10 @@ export default function GuitarPage() {
                     </Form>
                 }
             </FilterSlider>
+                {guitarSelected && <GuitarFormEdit
+                    guitar={guitarSelected}
+                    onClose={() => setGuitarSelected(undefined)}
+                    onSavePerson={() => editGuitarSave(guitarSelected)}/>}
             <Guitars guitars={values?.filter(g => g.kind.includes(search))}  onEditGuitar={editGuitar} title={"Guitars"}/>
 
         </div>
